f42de7de5e633cc71ba536bff91d33d86cc3313b
冀鹏旭 <1273692835@qq.com>
2018-07-10 21:08:26 +0800

    加了一行注解

--- gmall-order-web/src/main/java/com/atguigu/gmall/user/controller/OrederController.java
+++ gmall-order-web/src/main/java/com/atguigu/gmall/user/controller/OrederController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-
+//订单层
 @Controller
 public class OrederController {
     @Reference
